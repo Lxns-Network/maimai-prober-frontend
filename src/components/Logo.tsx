999a0bce07efff8cc3360be06a5454c3d5a677a3
Lxns <joinchang1206@gmail.com>
2024-02-19 15:47:29 +0800

    删除测试版标志

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,18 +1,12 @@
-import { Flex, Image, Badge, Text, rem } from '@mantine/core'
-import { useMediaQuery } from "@mantine/hooks";
+import { Flex, Image, Text } from '@mantine/core'
 
 export default function Logo() {
-  const small = useMediaQuery(`(max-width: ${rem(400)})`);
-
   return (
     <Flex id="logo" align="center" gap="xs">
       <Image alt="logo" src="/favicon.webp" width={32} height={32} />
       <Text fw={700} fz={18} truncate>
         maimai DX 查分器
       </Text>
-      <Badge variant="light" display={
-        small ? "none" : "flex"
-      }>测试版</Badge>
     </Flex>
   );
 }
