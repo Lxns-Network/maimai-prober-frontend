f14f02534bedff1001952db1bb916fa855a20dd4
Lxns <joinchang1206@gmail.com>
2024-05-14 09:59:50 +0800

    中二节奏成绩上传历史记录现在纵轴为成绩，不再为 Rating

--- src/components/Scores/chunithm/ScoreHistory.tsx
+++ src/components/Scores/chunithm/ScoreHistory.tsx
@@ -4,11 +4,22 @@ import { IconDatabaseOff } from "@tabler/icons-react";
 import { Area, AreaChart, CartesianGrid, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
 
 const ScoreHistoryChart = ({ scores }: { scores: ChunithmScoreProps[] }) => {
+  let ticks = [800000, 900000, 925000, 950000, 975000, 990000, 1000000, 1005000, 1007500, 1009000, 1010000];
+  let min = 1010000;
+  scores.forEach((score) => {
+    if (score.score < min) {
+      min = score.score;
+    }
+  });
+  ticks = ticks.filter((tick) => {
+    return tick >= Math.floor(min);
+  });
+
   return (
     <ResponsiveContainer width="100%" height={250}>
       <AreaChart data={scores}>
         <defs>
-          <linearGradient id="rating" x1="0" y1="0" x2="0" y2="1">
+          <linearGradient id="score" x1="0" y1="0" x2="0" y2="1">
             <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
             <stop offset="95%" stopColor="#8884d8" stopOpacity={0}/>
           </linearGradient>
@@ -18,8 +29,10 @@ const ScoreHistoryChart = ({ scores }: { scores: ChunithmScoreProps[] }) => {
           day: "numeric",
         })} fontSize={14} />
         <YAxis width={40} domain={([dataMin, dataMax]) => {
-          return [Math.max(Math.floor(dataMin) - 10, 0), Math.floor(dataMax) + 10]
-        }} fontSize={14} />
+          return [Math.max(Math.floor(dataMin) - 10000, 800000), Math.min(Math.floor(dataMax) + 10000, 1010000)]
+        }} ticks={ticks} fontSize={14} tickFormatter={
+          (value) => Math.floor(value / 1000) + "k"
+        } />
         <CartesianGrid strokeDasharray="3 3" />
         <Tooltip content={(props) => {
           if (!props.active || !props.payload || props.payload.length < 1) return null;
@@ -27,12 +40,12 @@ const ScoreHistoryChart = ({ scores }: { scores: ChunithmScoreProps[] }) => {
           return (
             <Card p="xs" withBorder fz="sm">
               <Text>{new Date(payload.upload_time).toLocaleDateString()}</Text>
-              <Text c="#8884d8">Rating: {Math.floor(payload.rating * 100) / 100}</Text>
-              <Text><NumberFormatter value={payload.score || 0} thousandSeparator /></Text>
+              <Text c="#8884d8"><NumberFormatter value={payload.score || 0} thousandSeparator /></Text>
+              <Text>Rating: {Math.floor(payload.rating * 100) / 100}</Text>
             </Card>
           )
         }} />
-        <Area type="monotone" dataKey="rating" stroke="#8884d8" fillOpacity={1} fill="url(#rating)" />
+        <Area type="monotone" dataKey="score" stroke="#8884d8" fillOpacity={1} fill="url(#score)" />
       </AreaChart>
     </ResponsiveContainer>
   )
