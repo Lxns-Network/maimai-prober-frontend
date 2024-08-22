69c2e382df94afc2ae2fb7c490ef74bc1929a1d2
Lxns <joinchang1206@gmail.com>
2024-08-22 12:05:52 +0800

    成绩详情现在支持查询舞萌 DX 的 DX 分数与中二节奏的 Over Power

--- src/components/Scores/chunithm/ScoreModal.tsx
+++ src/components/Scores/chunithm/ScoreModal.tsx
@@ -2,10 +2,10 @@ import { ChunithmScoreProps } from "./Score.tsx";
 import {
   Avatar,
   Box,
-  Card, Center,
+  Card, Center, Grid,
   Group,
   Image,
-  NumberFormatter, Rating,
+  NumberFormatter, Paper, Rating,
   rem,
   Text,
 } from "@mantine/core";
@@ -14,113 +14,19 @@ import { getDifficulty, ChunithmSongProps, ChunithmDifficultyProps } from "../..
 import { IconPhotoOff } from "@tabler/icons-react";
 import { PhotoView } from "react-photo-view";
 import { CustomMarquee } from "../../CustomMarquee.tsx";
-import classes from "./ScoreModal.module.css";
+import classes from "../ScoreModal.module.css";
 import { SongDisabledIndicator } from "../../SongDisabledIndicator.tsx";
 import { ASSET_URL } from "../../../main.tsx";
-import { useEffect, useState } from "react";
-
-const ChunithmWorldsEndScoreModalContent = ({ score, song }: { score: ChunithmScoreProps, song: ChunithmSongProps }) => {
-  const difficulty = getDifficulty(song, score.level_index);
-  if (!difficulty) {
-    return null;
-  }
-
-  return (
-    <>
-      <Group wrap="nowrap">
-        <PhotoView src={`${ASSET_URL}/chunithm/jacket/${difficulty.origin_id}.png`}>
-          <Avatar src={`${ASSET_URL}/chunithm/jacket/${difficulty.origin_id}.png!webp`} size={94} radius="md">
-            <IconPhotoOff />
-          </Avatar>
-        </PhotoView>
-        <div style={{ flex: 1 }}>
-          <CustomMarquee>
-            <Text fz="lg" fw={500}>{song.title}</Text>
-          </CustomMarquee>
-          <Text fz="xs" c="dimmed">曲目 ID：{song.id}</Text>
-          <Text fz="xs" c="dimmed" mb={8}>原曲 ID：{difficulty.origin_id}</Text>
-          <Group gap="xs">
-            {score.clear === "failed" && (
-              <Image
-                src={`/assets/chunithm/music_icon/failed.webp`}
-                w={rem(94)}
-              />
-            )}
-            {score.clear === "clear" && !score.full_combo && (
-              <Image
-                src={`/assets/chunithm/music_icon/clear.webp`}
-                w={rem(94)}
-              />
-            )}
-            {score.full_combo && (
-              <Image
-                src={`/assets/chunithm/music_icon/${score.full_combo}.webp`}
-                w={rem(94)}
-              />
-            )}
-            {score.full_chain && (
-              <Image
-                src={`/assets/chunithm/music_icon/${score.full_chain}.webp`}
-                w={rem(94)}
-              />
-            )}
-          </Group>
-        </div>
-        <Card w={60} h={54} p={0} radius="md" style={{
-          border: "2px solid rgb(14, 45, 56)",
-        }}>
-          <Center pb={1} style={{
-            backgroundColor: "rgb(14, 45, 56)",
-          }}>
-            <Rating count={difficulty.star} value={5} size={10} readOnly />
-          </Center>
-          <Text className={classes.worldsEndText} fz={24} fw={700} ta="center">
-            {difficulty.kanji}
-          </Text>
-        </Card>
-      </Group>
-      {score.score < 0 ? (
-        <Text fz="md" mt="md">
-          你还未游玩此谱面，或未上传至查分器。
-        </Text>
-      ) : (
-        <>
-          <Group mt="md">
-            <Image
-              src={`/assets/chunithm/music_rank/${score.rank}.webp`}
-              w={rem(94)}
-            />
-            <Box>
-              <Text fz="xs" c="dimmed">成绩</Text>
-              <Text fz={rem(24)} style={{ lineHeight: rem(24) }}>
-                <NumberFormatter value={score.score || 0} thousandSeparator />
-              </Text>
-            </Box>
-          </Group>
-          <Group mt="md">
-            {score.play_time && (
-              <Box mr={12}>
-                <Text fz="xs" c="dimmed">游玩时间</Text>
-                <Text fz="md">
-                  {new Date(score.play_time || "").toLocaleString()}
-                </Text>
-              </Box>
-            )}
-            <Box>
-              <Text fz="xs" c="dimmed">上传时间</Text>
-              <Text fz="md">
-                {new Date(score.upload_time || "").toLocaleString()}
-              </Text>
-            </Box>
-          </Group>
-        </>
-      )}
-    </>
-  )
-}
+import { useContext, useEffect, useState } from "react";
+import { ApiContext } from "../../../App.tsx";
+import { useMediaQuery } from "@mantine/hooks";
 
 export const ChunithmScoreModalContent = ({ score, song }: { score: ChunithmScoreProps, song: ChunithmSongProps }) => {
   const [difficulty, setDifficulty] = useState<ChunithmDifficultyProps | null>(null);
+
+  const context = useContext(ApiContext);
+  const songList = context.songList.chunithm;
+  const small = useMediaQuery('(max-width: 30rem)');
 
   useEffect(() => {
     if (!song) return;
@@ -129,16 +35,13 @@ export const ChunithmScoreModalContent = ({ score, song }: { score: ChunithmScor
   }, [song]);
 
   if (!song) return;
-  if (song.id >= 8000) {
-    return <ChunithmWorldsEndScoreModalContent score={score} song={song} />
-  }
 
   return (
     <>
       <Group wrap="nowrap">
         <SongDisabledIndicator disabled={song.disabled}>
-          <PhotoView src={`${ASSET_URL}/chunithm/jacket/${song.id}.png`}>
-            <Avatar src={`${ASSET_URL}/chunithm/jacket/${song.id}.png!webp`} size={94} radius="md">
+          <PhotoView src={`${ASSET_URL}/chunithm/jacket/${difficulty?.origin_id ?? songList.getSongResourceId(song.id)}.png`}>
+            <Avatar src={`${ASSET_URL}/chunithm/jacket/${difficulty?.origin_id ?? songList.getSongResourceId(song.id)}.png!webp`} size={94} radius="md">
               <IconPhotoOff />
             </Avatar>
           </PhotoView>
@@ -148,6 +51,7 @@ export const ChunithmScoreModalContent = ({ score, song }: { score: ChunithmScor
             <Text fz="lg" fw={500}>{song.title}</Text>
           </CustomMarquee>
           <Text fz="xs" c="dimmed" mb={8}>曲目 ID：{song.id}</Text>
+          {difficulty?.origin_id && <Text fz="xs" c="dimmed" mt={-8} mb={8}>原曲 ID：{difficulty?.origin_id}</Text>}
           <Group gap="xs">
             {score.clear === "failed" && (
               <Image
@@ -175,16 +79,31 @@ export const ChunithmScoreModalContent = ({ score, song }: { score: ChunithmScor
             )}
           </Group>
         </div>
-        <Card w={54} h={38} p={0} radius="md" withBorder style={{
-          border: `2px solid ${getScoreSecondaryColor("chunithm", score.level_index || 0)}`,
-          backgroundColor: getScoreCardBackgroundColor("chunithm", score.level_index || 0)
-        }}>
-          <Text size="xl" fw={500} ta="center" c="white" style={{
-            lineHeight: rem(34),
+        {difficulty?.star ? (
+          <Card w={60} h={54} p={0} radius="md" style={{
+            border: "2px solid rgb(14, 45, 56)",
           }}>
-            {difficulty ? difficulty.level_value.toFixed(1) : "?"}
-          </Text>
-        </Card>
+            <Center pb={1} style={{
+              backgroundColor: "rgb(14, 45, 56)",
+            }}>
+              <Rating count={difficulty.star} value={5} size={10} readOnly />
+            </Center>
+            <Text className={classes.worldsEndText} fz={24} fw={700} ta="center">
+              {difficulty.kanji}
+            </Text>
+          </Card>
+        ) : (
+          <Card w={54} h={38} p={0} radius="md" withBorder style={{
+            border: `2px solid ${getScoreSecondaryColor("chunithm", score.level_index || 0)}`,
+            backgroundColor: getScoreCardBackgroundColor("chunithm", score.level_index || 0)
+          }}>
+            <Text size="xl" fw={500} ta="center" c="white" style={{
+              lineHeight: rem(34),
+            }}>
+              {difficulty ? difficulty.level_value.toFixed(1) : "?"}
+            </Text>
+          </Card>
+        )}
       </Group>
       {score.score < 0 ? (
         <Text fz="md" mt="md">
@@ -204,28 +123,42 @@ export const ChunithmScoreModalContent = ({ score, song }: { score: ChunithmScor
               </Text>
             </Box>
           </Group>
-          <Group mt="md">
-            <Box>
-              <Text fz="xs" c="dimmed">Rating</Text>
-              <Text fz="md">
-                {Math.floor(score.rating * 100) / 100}
-              </Text>
-            </Box>
-            {score.play_time && (
-              <Box>
-                <Text fz="xs" c="dimmed">游玩时间</Text>
+          <Grid mt="md">
+            <Grid.Col span={6}>
+              <Paper className={classes.subparameters}>
+                <Text fz="xs" c="dimmed">Rating</Text>
                 <Text fz="md">
-                  {new Date(score.play_time || "").toLocaleString()}
+                  {song.id >= 8000 ? "-" : Math.floor(score.rating * 100) / 100}
                 </Text>
-              </Box>
+              </Paper>
+            </Grid.Col>
+            <Grid.Col span={6}>
+              <Paper className={classes.subparameters}>
+                <Text fz="xs" c="dimmed">Over Power</Text>
+                <Text fz="md">
+                  {Math.floor(score.over_power * 100) / 100}%
+                </Text>
+              </Paper>
+            </Grid.Col>
+            {score.play_time && (
+              <Grid.Col span={small ? 12 : 6}>
+                <Paper className={classes.subparameters}>
+                  <Text fz="xs" c="dimmed">游玩时间</Text>
+                  <Text fz="md">
+                    {new Date(score.play_time || "").toLocaleString()}
+                  </Text>
+                </Paper>
+              </Grid.Col>
             )}
-            <Box>
-              <Text fz="xs" c="dimmed">上传时间</Text>
-              <Text fz="md">
-                {new Date(score.upload_time || "").toLocaleString()}
-              </Text>
-            </Box>
-          </Group>
+            <Grid.Col span={small ? 12 : 6}>
+              <Paper className={classes.subparameters}>
+                <Text fz="xs" c="dimmed">上传时间</Text>
+                <Text fz="md">
+                  {new Date(score.upload_time || "").toLocaleString()}
+                </Text>
+              </Paper>
+            </Grid.Col>
+          </Grid>
         </>
       )}
     </>
--- src/components/Scores/maimai/ScoreModal.tsx
+++ src/components/Scores/maimai/ScoreModal.tsx
@@ -3,137 +3,65 @@ import {
   Avatar,
   Badge,
   Box,
-  Card,
+  Card, Grid,
   Group,
-  Image,
+  Image, Paper,
   rem,
   Text,
 } from "@mantine/core";
 import { getScoreCardBackgroundColor, getScoreSecondaryColor } from "../../../utils/color.tsx";
-import { getDifficulty, MaimaiDifficultyProps, MaimaiSongProps } from "../../../utils/api/song/maimai.tsx";
+import {
+  getDifficulty,
+  MaimaiDifficultyProps,
+  MaimaiNotesProps,
+  MaimaiSongProps
+} from "../../../utils/api/song/maimai.tsx";
 import { IconPhotoOff } from "@tabler/icons-react";
 import { PhotoView } from "react-photo-view";
 import { CustomMarquee } from "../../CustomMarquee.tsx";
 import { SongDisabledIndicator } from "../../SongDisabledIndicator.tsx";
 import { ASSET_URL } from "../../../main.tsx";
-import { useEffect, useState } from "react";
-
-const MaimaiUtageScoreModalContent = ({ score, song }: { score: MaimaiScoreProps, song: MaimaiSongProps }) => {
-  const difficulty = getDifficulty(song, "utage", 0);
-
-  return (
-    <>
-      <Group wrap="nowrap">
-        <PhotoView src={`${ASSET_URL}/maimai/jacket/${song.id%10000}.png`}>
-          <Avatar src={`${ASSET_URL}/maimai/jacket/${song.id%10000}.png!webp`} size={94} radius="md">
-            <IconPhotoOff />
-          </Avatar>
-        </PhotoView>
-        <div style={{ flex: 1 }}>
-          <Group gap={6}>
-            {difficulty.is_buddy && (
-              <Badge variant="filled" color="rgb(73, 9, 10)" size="sm">BUDDY</Badge>
-            )}
-          </Group>
-          <CustomMarquee>
-            <Text fz="lg" fw={500} mt={2}>{song.title}</Text>
-          </CustomMarquee>
-          <Text fz="xs" c="dimmed" mb={2}>曲目 ID：{song.id}</Text>
-          <Group gap={0} ml={-3}>
-            <Image
-              src={`/assets/maimai/music_icon/${score.fc || "blank"}.webp`}
-              w={rem(30)}
-            />
-            <Image
-              src={`/assets/maimai/music_icon/${score.fs || "blank"}.webp`}
-              w={rem(30)}
-            />
-          </Group>
-        </div>
-        <Card w={54} h={38} p={0} radius="md" withBorder style={{
-          border: `2px solid rgb(204, 12, 175)`,
-          backgroundColor: "rgb(234, 61, 232)",
-        }}>
-          <Text size="xl" fw={500} ta="center" c="white" style={{
-            lineHeight: rem(34),
-          }}>
-            {difficulty.level}
-          </Text>
-        </Card>
-      </Group>
-      {score.achievements != -1 ? (
-        <>
-          <Group mt="md">
-            <Image
-              src={`/assets/maimai/music_rank/${score?.rate}.webp`}
-              w={rem(64)}
-            />
-            <Box>
-              <Text fz="xs" c="dimmed">达成率</Text>
-              <Text fz={rem(24)} style={{ lineHeight: rem(24) }}>
-                {parseInt(String(score.achievements))}
-                <span style={{ fontSize: rem(16) }}>.{
-                  (String(score?.achievements).split(".")[1] || "0").padEnd(4, "0")
-                }%</span>
-              </Text>
-            </Box>
-          </Group>
-          <Group mt="md">
-            {score.play_time && (
-              <Box mr={12}>
-                <Text fz="xs" c="dimmed">游玩时间</Text>
-                <Text fz="md">
-                  {new Date(score.play_time || "").toLocaleString()}
-                </Text>
-              </Box>
-            )}
-            <Box>
-              <Text fz="xs" c="dimmed">上传时间</Text>
-              <Text fz="md">
-                {new Date(score.upload_time || "").toLocaleString()}
-              </Text>
-            </Box>
-          </Group>
-        </>
-      ) : (
-        <Text fz="md" mt="md">
-          你还未游玩此谱面，或未上传至查分器。
-        </Text>
-      )}
-    </>
-  )
-}
+import { useContext, useEffect, useState } from "react";
+import { useMediaQuery } from "@mantine/hooks";
+import classes from "../ScoreModal.module.css";
+import { ApiContext } from "../../../App.tsx";
 
 export const MaimaiScoreModalContent = ({ score, song }: { score: MaimaiScoreProps, song: MaimaiSongProps }) => {
   const [difficulty, setDifficulty] = useState<MaimaiDifficultyProps | null>(null);
+  const [level, setLevel] = useState(score.level);
+
+  const context = useContext(ApiContext);
+  const songList = context.songList.maimai;
+  const small = useMediaQuery('(max-width: 30rem)');
+
+  const levelIndex = score.type !== "utage" ? score.level_index : 5;
 
   useEffect(() => {
     if (!song) return;
 
-    setDifficulty(getDifficulty(song, score.type, score.level_index));
+    const difficulty = getDifficulty(song, score.type, score.level_index);
+    if (!difficulty) return;
+    setDifficulty(difficulty);
+    if (score.type === "utage") return;
+    setLevel(difficulty.level_value.toFixed(1));
   }, [song]);
 
   if (!song) return;
-  if (song.id > 100000) {
-    return <MaimaiUtageScoreModalContent score={score} song={song} />
-  }
 
   return (
     <>
       <Group wrap="nowrap">
         <SongDisabledIndicator disabled={song.disabled}>
-          <PhotoView src={`${ASSET_URL}/maimai/jacket/${song.id}.png`}>
-            <Avatar src={`${ASSET_URL}/maimai/jacket/${song.id}.png!webp`} size={94} radius="md">
+          <PhotoView src={`${ASSET_URL}/maimai/jacket/${songList.getSongResourceId(song.id)}.png`}>
+            <Avatar src={`${ASSET_URL}/maimai/jacket/${songList.getSongResourceId(song.id)}.png!webp`} size={94} radius="md">
               <IconPhotoOff />
             </Avatar>
           </PhotoView>
         </SongDisabledIndicator>
         <div style={{ flex: 1 }}>
-          {score.type === "standard" ? (
-            <Badge variant="filled" color="blue" size="sm">标准</Badge>
-          ) : (
-            <Badge variant="filled" color="orange" size="sm">DX</Badge>
-          )}
+          {score.type === "standard" && <Badge variant="filled" color="blue" size="sm">标准</Badge>}
+          {score.type === "dx" && <Badge variant="filled" color="orange" size="sm">DX</Badge>}
+          {difficulty?.is_buddy && <Badge variant="filled" color="rgb(73, 9, 10)" size="sm">BUDDY</Badge>}
           <CustomMarquee>
             <Text fz="lg" fw={500} mt={2}>{song.title}</Text>
           </CustomMarquee>
@@ -150,13 +78,13 @@ export const MaimaiScoreModalContent = ({ score, song }: { score: MaimaiScorePro
           </Group>
         </div>
         <Card w={54} h={38} p={0} radius="md" withBorder style={{
-          border: `2px solid ${getScoreSecondaryColor("maimai", score.level_index)}`,
-          backgroundColor: getScoreCardBackgroundColor("maimai", score.level_index),
+          border: `2px solid ${getScoreSecondaryColor("maimai", levelIndex)}`,
+          backgroundColor: getScoreCardBackgroundColor("maimai", levelIndex),
         }}>
           <Text size="xl" fw={500} ta="center" c="white" style={{
             lineHeight: rem(34),
           }}>
-            {difficulty ? difficulty.level_value.toFixed(1) : "?"}
+            {level}
           </Text>
         </Card>
       </Group>
@@ -177,28 +105,76 @@ export const MaimaiScoreModalContent = ({ score, song }: { score: MaimaiScorePro
               </Text>
             </Box>
           </Group>
-          <Group mt="md">
-            <Box>
-              <Text fz="xs" c="dimmed">DX Rating</Text>
-              <Text fz="md">
-                {parseInt(String(score.dx_rating))}
-              </Text>
-            </Box>
-            {score.play_time && (
-              <Box>
-                <Text fz="xs" c="dimmed">游玩时间</Text>
-                <Text fz="md">
-                  {new Date(score.play_time || "").toLocaleString()}
+          <Grid mt="md">
+            <Grid.Col span={6}>
+              <Paper className={classes.subparameters}>
+                <Text fz="xs" c="dimmed">DX Rating</Text>
+                <Text>
+                  {score.type === "utage" ? "-" : parseInt(String(score.dx_rating))}
                 </Text>
-              </Box>
+              </Paper>
+            </Grid.Col>
+            <Grid.Col span={6}>
+              <Paper className={classes.subparameters}>
+                <Group wrap="nowrap" gap="xs">
+                  <Text fz="xs" c="dimmed">DX 分数</Text>
+                  <Group wrap="nowrap" h={12} gap={0}>
+                    {(() => {
+                      if (!difficulty || !difficulty.notes) return null;
+
+                      let percentage: number;
+                      if (difficulty.is_buddy) {
+                        percentage = Math.floor((score.dx_score / (difficulty.notes["left" as keyof MaimaiNotesProps] + difficulty.notes["right" as keyof MaimaiNotesProps]) * 3) * 100);
+                      } else {
+                        percentage = Math.floor((score.dx_score / (difficulty.notes.total * 3)) * 100);
+                      }
+                      let count = 0;
+                      let rate = 1;
+
+                      if (percentage >= 97) {
+                        count = 5;
+                        rate = 3;
+                      } else if (percentage >= 95) {
+                        count = 4;
+                        rate = 2;
+                      } else if (percentage >= 93) {
+                        count = 3;
+                        rate = 2;
+                      } else if (percentage >= 90) {
+                        count = 2;
+                      } else if (percentage >= 85) {
+                        count = 1;
+                      }
+
+                      return Array.from({ length: count }).map((_, index) => (
+                        <Image key={index} src={`/assets/maimai/dx_score/${rate}.webp`} h="100%" />
+                      ));
+                    })()}
+                  </Group>
+                </Group>
+                {difficulty && difficulty.notes ? <Text>
+                  {score.dx_score}
+                  <span style={{ fontSize: 12, marginLeft: 4 }}>
+                    / {difficulty.notes.total * 3}
+                  </span>
+                </Text> : <Text>{score.dx_score}</Text>}
+              </Paper>
+            </Grid.Col>
+            {score.play_time && (
+              <Grid.Col span={small ? 12 : 6}>
+                <Paper className={classes.subparameters}>
+                  <Text fz="xs" c="dimmed">游玩时间</Text>
+                  <Text>{new Date(score.play_time || "").toLocaleString()}</Text>
+                </Paper>
+              </Grid.Col>
             )}
-            <Box>
-              <Text fz="xs" c="dimmed">上传时间</Text>
-              <Text fz="md">
-                {new Date(score.upload_time || "").toLocaleString()}
-              </Text>
-            </Box>
-          </Group>
+            <Grid.Col span={small ? 12 : 6}>
+              <Paper className={classes.subparameters}>
+                <Text fz="xs" c="dimmed">上传时间</Text>
+                <Text fz="md">{new Date(score.upload_time || "").toLocaleString()}</Text>
+              </Paper>
+            </Grid.Col>
+          </Grid>
         </>
       ) : (
         <Text fz="md" mt="md">
