4346f068a6467f97fe543e5da9ab103934237abe
Lxns <joinchang1206@gmail.com>
2024-09-04 22:46:14 +0800

    滚动文字现在支持支持鼠标悬停自动暂停

--- src/components/Marquee.tsx
+++ src/components/Marquee.tsx
@@ -1,58 +1,59 @@
-import { Children, ReactNode, useEffect, useLayoutEffect, useState } from "react";
+import { Children, ReactNode, useEffect, useLayoutEffect, useRef, useState } from "react";
 import { Group } from "@mantine/core";
-import { useHover } from "@mantine/hooks";
+import { useHoverDirty, useInterval } from "react-use";
 
 interface MarqueeProps {
   speed?: number;
-  delay?: number;
-  interval?: number;
-  pauseOnHover?: boolean;
+  startDelay?: number; // 开始滚动的延迟
+  intervalDelay?: number; // 滚动间隔
+  pauseOnHover?: boolean; // 鼠标悬停暂停滚动
   children: ReactNode;
   props?: any;
 }
 
-export const Marquee = ({ speed = 0.5, delay = 1000, interval = 1000, pauseOnHover = true, children, ...props }: MarqueeProps) => {
-  const { hovered, ref } = useHover();
-  const [isScrolling, setIsScrolling] = useState(false);
+export const Marquee = ({ speed = 0.5, startDelay = 1000, intervalDelay = 1000, pauseOnHover = true, children, ...props }: MarqueeProps) => {
+  const [isScrolling, setIsScrolling] = useState(false); // 是否需要滚动
+  const [isPaused, setIsPaused] = useState(false); // 是否暂停滚动
   const [translateX, setTranslateX] = useState(0);
   const [direction, setDirection] = useState(1);
+  const [delay, setDelay] = useState(startDelay); // 滚动延迟
+  const ref = useRef<HTMLDivElement>(null);
+  const isHovering = useHoverDirty(ref); // 是否鼠标悬停
+
+  useInterval(
+    () => {
+      setDelay(10);
+      setTranslateX((prev) => {
+        if (!ref.current) return 0;
+
+        const newTranslateX = prev - direction * speed;
+        const maxTranslateX = ref.current.scrollWidth - ref.current.clientWidth;
+
+        if (newTranslateX <= -maxTranslateX - speed || newTranslateX >= speed) {
+          setDirection((prev) => -prev);
+        }
+
+        return newTranslateX;
+      });
+    },
+    isScrolling && !isPaused ? delay : null
+  );
+
+  useEffect(() => {
+    setDelay(intervalDelay);
+  }, [direction]);
 
   useEffect(() => {
     if (!isScrolling) {
       setTranslateX(0);
-      return;
     }
-
-    let isPaused = hovered && pauseOnHover;
-
-    const scroll = () => {
-      if (!isPaused) {
-        setTranslateX((prev) => {
-          if (!ref.current) return 0;
-
-          const newTranslateX = prev - direction * speed;
-          const maxTranslateX = ref.current.scrollWidth - ref.current.clientWidth;
-
-          if (newTranslateX <= -maxTranslateX - speed || newTranslateX >= speed) {
-            isPaused = true;
-            setDirection((prev) => -prev);
-
-            setTimeout(() => {
-              isPaused = false;
-            }, interval);
-          }
-
-          return newTranslateX;
-        });
-      }
-    };
-
-    const intervalId = setInterval(scroll, 10);
-
-    return () => clearInterval(intervalId);
-  }, [isScrolling, direction, hovered]);
+  }, [isScrolling]);
 
   useLayoutEffect(() => {
+    if (pauseOnHover) {
+      setIsPaused(isHovering);
+    }
+
     if (ref.current && ref.current.scrollWidth > ref.current.clientWidth) {
       setTimeout(() => {
         setIsScrolling(true);
