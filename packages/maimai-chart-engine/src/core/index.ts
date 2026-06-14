4a6332e1a72601733687241f7bb81726d9a13fb0
Amatsuka <45129539+wsyzxjn@users.noreply.github.com>
2026-06-15 04:46:02 +0800

    feat: 新增 ma2 解析器并更新控制组件以支持谱面导入

--- packages/maimai-chart-engine/src/core/index.ts
+++ packages/maimai-chart-engine/src/core/index.ts
@@ -1,6 +1,8 @@
 export {
   parseSimaiChart,
+  parseMa2Chart,
   getAvailableDifficulties,
+  type ChartFileType,
   default as ChartParser,
 } from "./parser/ChartParser";
 export { AudioManager, type AudioManagerConfig } from "./audio/AudioManager";
